e8ea143a76f03e46608aa47a84c1c5ffdd87def4
Rento Yabuki <ryabuki@c6r2s10.42tokyo.jp>
2025-04-21 21:22:11 +0900

    norm:philo.h

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,11 +6,11 @@
 /*   By: ryabuki <ryabuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 00:30:36 by yabukirento       #+#    #+#             */
-/*   Updated: 2025/04/20 22:53:32 by ryabuki          ###   ########.fr       */
+/*   Updated: 2025/04/21 21:20:53 by ryabuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef	PHILO_H
+#ifndef PHILO_H
 # define PHILO_H
 
 # include <stdbool.h>
@@ -22,14 +22,14 @@
 # include <pthread.h>
 # include <stddef.h>
 
-typedef struct	s_info	t_info;
-typedef struct	s_philo	t_philo;
+typedef struct s_info	t_info;
+typedef struct s_philo	t_philo;
 
 struct	s_info
 {
 	bool			flag_finish;
-	int 			num_philo;
-	long long 		time_start;
+	int				num_philo;
+	long long		time_start;
 	int				time_to_die;
 	int				time_to_eat;
 	int				time_to_sleep;
@@ -43,31 +43,31 @@ struct	s_info
 struct	s_philo
 {
 	int				count_eat;
-	long long 		last_eat_time;
+	long long		last_eat_time;
 	int				index;
 	pthread_t		thread;
 	t_info			*info;
 };
 
-void	*ft_monitor_routine(void *arg);
-void	*ft_philo_routine(void *arg);
+void		*ft_monitor_routine(void *arg);
+void		*ft_philo_routine(void *arg);
 
-bool	ft_init_info(int argc, char **argv, t_info *info);
-bool	ft_init_philos(t_philo **philos, t_info *info);
+bool		ft_init_info(int argc, char **argv, t_info *info);
+bool		ft_init_philos(t_philo **philos, t_info *info);
 
-int		ft_atoi(const char *str);
-int		ft_isdigit(int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_atoi(const char *str);
+int			ft_isdigit(int c);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 
-void	ft_print_status(t_info *info, char *act, int index_philo);
+void		ft_print_status(t_info *info, char *act, int index_philo);
 
-long long get_current_time(void);
+long long	get_current_time(void);
 
-void	ft_free_forks(t_info *info);
-void	ft_free_info_without_forks(t_info *info);
-void	ft_free_all(t_info **info_ptr, t_philo **philos_ptr);
+void		ft_free_forks(t_info *info);
+void		ft_free_info_without_forks(t_info *info);
+void		ft_free_all(t_info **info_ptr, t_philo **philos_ptr);
 
-bool	get_finish(t_info *info);
-void	set_finish(t_info *info, bool value);
+bool		get_finish(t_info *info);
+void		set_finish(t_info *info, bool value);
 
 #endif
