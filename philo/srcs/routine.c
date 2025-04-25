b4c789c7dff55201948b05123ef12e35ddf9dc7f
Rento Yabuki <ryabuki@c6r1s14.42tokyo.jp>
2025-04-25 18:36:40 +0900

    sleep時間を調整しました

--- philo/srcs/routine.c
+++ philo/srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: ryabuki <ryabuki@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 22:27:55 by ryabuki           #+#    #+#             */
-/*   Updated: 2025/04/25 18:09:48 by ryabuki          ###   ########.fr       */
+/*   Updated: 2025/04/25 18:35:28 by ryabuki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,35 +20,13 @@ unsigned int	my_rand(unsigned int *seed)
 
 static void	ft_initial_wait(t_philo *philo)
 {
-	// unsigned int	seed;
-	// int				delay_base;
-	// int				random_offset;
-
-	// seed = (unsigned int)(philo->index * 1234 + (uintptr_t)philo);
-	// delay_base = philo->index * 1000;
-	// random_offset = my_rand(&seed) % 1000;
 	if (philo->info->num_philo % 2 == 0)
 	{
 		if (philo->index % 2 == 0)
-		{
 			usleep(2000);
-		}
-		else
-		{
-			usleep(0);
-		}
 	}
-	else if (philo->info->num_philo > 10)
-	{
-		usleep(philo->index * 100);
-	}
-	else
-	{
-		usleep(philo->index * 1000);
-	}
-	// if (philo->index % 2 == 1)
-	// 	delay_base += 3000;
-	// usleep((delay_base + random_offset) * 10);
+	else 
+		usleep(philo->index * 200);
 }
 
 void	*pr(void *arg)
@@ -68,7 +46,7 @@ void	*pr(void *arg)
 		usleep((philo->info->time_to_sleep) * 1000);
 		ft_print_status(philo->info, "think", philo->index);
 		think_time = philo->info->time_to_eat - philo->info->time_to_sleep;
-		if (think_time > 0)
+		if (think_time >= 0)
 			usleep((think_time + 1) * 1000);
 	}
 	return (NULL);
